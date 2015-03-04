393185f281f514f676b004ea7df466014021b269
Nikolay Yaremko <makiwara@humanemagica.com>
2015-03-04 05:05:48 +0500

    Sketched the approach

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # simplecalendar
-
 This is a simplecalendar test case.
 Work is in progress.
+
+## jQuery Plugin Approach
+I choose to implement calendar as a jQuery plugin in order to follow jQuery extension guidelines; this approach allows various ways to install calendar and removes the need to customize labels/inputs via configuration objects.
+
+However, it is possible to create shorthands that would generate such labels/inputs. 
+
+## Calendar as a singleton
+I choose to go with singleton-style approach to calendar behavior. It is not possible to show more than one calendar pop-up at the time: I consider this behavior to be clear and intuitive for the user; multiple pop-ups might clatter the screen.
+
+## Styling
+I choose to put all styles in simplecalendar.css file, because such an approach will allow better customization of visual style and subtle animation behavior.
+
+The CSS is not minimized because it would be better to glue it with other style files and to minimize/obfucate them using Grunt or other automation.
+
+Another way to ged rid of CSS is to hardcode it into javascript library. I consider this path to be inefficient in terms of support and future re-use.
--- js/simplecalendar.js
+++ js/simplecalendar.js
@@ -1,18 +1,41 @@
 (function($) {
+
+    var $calendar = false;
  
     $.fn.simplecalendar = function(options) {
         var defaults = {
-            visible: false, // show calendar at start (at first encounter)
+            prerender: true, // prerender calendar on init? TODO set to false
+            visible: true, // show calendar at start (at first encounter)? TODO set to false
             prevWeeks: 2,   // how many rows to show in the past
             nextDays: 180,  // for how many days in the future to span
             disabledDays: [6], // 0–Monday...6–Sunday
             disabledDates: ['11-05-2015'] // 'dd-mm-yyyy'
         };
-        var settings = $.extend( {}, defaults, options );
+        var settings = $.extend({}, defaults, options);
+
+        if (settings.prerender) prerender();
         
+        var isVisible = false
         return this.each(function() {
-            // TODO initialize
+            if (settings.visible && !isVisible) {
+                isVisible = true;
+                open(this);
+            }
+            // TODO bind focus events
         });
     };
+
+    function prerender() {
+        if ($calendar !== false) return;
+        $calendar = $('div').html('HELLO');
+        // TODO render
+
+    }
+
+    function open(input) {
+        prerender();
+        // TODO gather input data
+        $calendar.prependTo(input).show();
+    }
  
 }( jQuery ));
